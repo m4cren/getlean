a3fd8b8b770dfb6d069a40529f441dbbcf0ccade
m4cren <renrenofficial@gmail.com>
2025-02-22 00:41:59 +0800

    ready to deploy

--- GetLean/vite.config.ts
+++ GetLean/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [react()],
-    server: {
-        host: '192.168.1.25',
-        port: 5173, // Change if needed
-    },
+    // server: {
+    //     host: '192.168.1.25',
+    //     port: 5173, // Change if needed
+    // },
 });
